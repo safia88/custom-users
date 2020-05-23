92d079ec2732c785a3f8485237e9a6334faa3dd1
Safia Ali <safialiusa@gmail.com>
2020-05-23 06:05:59 +0600

    fix template in htm

--- CustomUser/models.py
+++ CustomUser/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 # Create your models here.
 
-
+user=settings.AUTH_USER_MODEL
 class Customuser(AbstractUser):
     Homepage = models.URLField(null=True)
     Display_name = models.CharField(max_length=100, null=True)
